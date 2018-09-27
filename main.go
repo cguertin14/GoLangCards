be91af5161946b61444ee9beec122c3e33b4486d
guertz <charlesguertin@live.ca>
2018-09-27 10:57:22 -0400

    Variables

--- main.go
+++ main.go
@@ -2,7 +2,15 @@ package main
 
 import "fmt"
 
+var deco int
+
 func main() {
-	var card string = "Ace of spades"
+	//var card string = "Ace of spades"
+	card := "Ace of spades"
+	card = "Five of diamons"
+
+	deco = 5
+
+	fmt.Println(deco)
 	fmt.Println(card)
 }
